7c062e185965fc93f0c9e2e63868c175c35205a0
jdragonhx <jayden.dhx@gmail.com>
2025-07-27 04:14:24 +0000

    Jul 27, 2025, 4:14 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -3,7 +3,8 @@ function setup() {
   
 function draw() {
   circle(75, 200, 100);
-  circle(425, 200, 100);
+  circle(75, 200, 100);
+    circle(75, 200, 100)
   // Recap 1: Repeating Circles
 
   // Task 1: Colour Gradient
