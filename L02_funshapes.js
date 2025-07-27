9faa6cb89c0dd62f62d0d15440711e1a253ea197
jdragonhx <jayden.dhx@gmail.com>
2025-07-27 03:56:59 +0000

    Jul 27, 2025, 3:56 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -28,8 +28,7 @@ function draw() {
     // fill(0, 255, 0);
     // quad(120, 150, 180, 150, 180, 170, 120, 170);
 
-    noStroke()
-    
+    stroke
 
   // Challenge 1: An eye using ellipse() function
 
