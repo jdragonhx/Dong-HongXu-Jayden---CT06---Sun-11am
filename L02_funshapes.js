7821b275247385f5e737f416ff209b800428d298
jdragonhx <jayden.dhx@gmail.com>
2025-07-27 03:25:27 +0000

    Jul 27, 2025, 3:25 AM

--- L02_funshapes.js
+++ L02_funshapes.js
@@ -18,8 +18,8 @@ function draw() {
     // triangle(30, 75, 58, 20, 86, 75);
     // triangle(20, 40, 75, 40, 57, 32);
   
-    quad(20, 80, 80, 80, 120, 20, 120);
-    
+    quad(20, 80, 80, 80, 120, 20, 120); //rectangle
+
     
   // Challenge 1: An eye using ellipse() function
 
