39285f69c54e5af6bca23a932beb63ff01de4dfb
jdragonhx <jayden.dhx@gmail.com>
2025-08-03 03:25:03 +0000

    Aug 3, 2025, 3:25 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -1,4 +1,4 @@
-let spacing = 5;
+let spacing = 50;
 
 function setup() {
   createCanvas(600, 600);
@@ -8,7 +8,7 @@ function setup() {
 function draw() {                      
   for (let i = 0; i < 11; i++) {
     fill(0, 0, i*50)                              //loop                         instead of circle(75, 200, 100);
-    circle(50 + i*50, 200, 40);               //idk to be honest                        circle(225, 200, 100);
+    circle( + i*50, 200, 40);               //idk to be honest                        circle(225, 200, 100);
   }  
                                               // continues                              circle(375, 200, 100);
                                                         //                              circle -----------
