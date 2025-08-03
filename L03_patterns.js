28c307c5ff2e37d0ed46697916cb05650de5a269
jdragonhx <jayden.dhx@gmail.com>
2025-08-03 03:22:53 +0000

    Aug 3, 2025, 3:22 AM

--- L03_patterns.js
+++ L03_patterns.js
@@ -4,7 +4,7 @@ function setup() {
   }
     
 function draw() {                      
-  for (let i = 0; i < 11; i++) []
+  for (let i = 0; i < 11; i++)
     fill(0, 0, i*50)                              //loop                         instead of circle(75, 200, 100);
     circle(50 + i*50, 200, 40);               //idk to be honest                        circle(225, 200, 100);
     
