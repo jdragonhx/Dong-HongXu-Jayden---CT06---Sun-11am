63fcdcad4751896e0ad690f3dc7ad32c2c7a3aec
jdragonhx <jayden.dhx@gmail.com>
2025-08-17 04:24:10 +0000

    Aug 17, 2025, 4:24 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -59,7 +59,7 @@ function setup() {
 
 
 function keyPressed() { 
-    let count = floor(ramdom(5, 21));
+    let count = floor(random(5, 21));
     
     for(let i=0; i<count; i++) {
         // random postition
