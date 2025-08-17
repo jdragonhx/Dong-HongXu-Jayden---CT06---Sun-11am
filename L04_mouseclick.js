611f03abbf8ae2f80b9db47386c29ae1b3d20510
jdragonhx <jayden.dhx@gmail.com>
2025-08-17 03:19:56 +0000

    Aug 17, 2025, 3:19 AM

--- L04_mouseclick.js
+++ L04_mouseclick.js
@@ -7,26 +7,26 @@ function setup() {
 
 }
 
-function draw() {
-    // // for (let i=0; i<5; i++) {
-    // //     fill(0, i*50, 0)
-    // //     circle(50+ i*50, 50 + i*50, 50)
-    // }
-    fill(shapeColor)
-    circle(width/2, height/2, 100);
-}
+// function draw() {
+//     // // for (let i=0; i<5; i++) {
+//     // //     fill(0, i*50, 0)
+//     // //     circle(50+ i*50, 50 + i*50, 50)
+//     // }
+//     fill(shapeColor)
+//     circle(width/2, height/2, 100);
+// }
 
-function mousePressed() {
-    shapeColor = color(random(255), random(255), random(255));
-}
+// function mousePressed() {
+//     shapeColor = color(random(255), random(255), random(255));
+// }
 
-function mouseReleased() {
-    // shapeColor = 'lime'
-    // shapeColor = color(random(255), random(255), random(255));
-    shapeColor = 'white';
-}
+// function mouseReleased() {
+//     // shapeColor = 'lime'
+//     // shapeColor = color(random(255), random(255), random(255));
+//     shapeColor = 'white';
+// }
 
-function mouseMoved() {
-    fill(random(255), random(255), random(255))
-    circle(mouseX, mouseY, 50)
-}
+// function mouseMoved() {
+//     fill(random(255), random(255), random(255))
+//     circle(mouseX, mouseY, 50)
+// }
