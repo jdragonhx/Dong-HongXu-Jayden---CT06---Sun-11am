c5aea5d9c01e98a652a0a32fba8aac5aabb8be35
jdragonhx <jayden.dhx@gmail.com>
2025-08-17 03:23:32 +0000

    Aug 17, 2025, 3:23 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -1,4 +1,12 @@
 // write your codes here
+let shapeColor = 'blue'; //initial color
+
+function setup() {
+    createCanvas(600, 600);
+    background(220);
+
 function mousePressed() {
     fill(random(255), random(255), random(255))
+}
+
 }
