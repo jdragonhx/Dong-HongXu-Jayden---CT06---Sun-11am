e874c1a0e641c846de7d4299afffaa2d1a2cea91
jdragonhx <jayden.dhx@gmail.com>
2025-08-17 04:14:57 +0000

    Aug 17, 2025, 4:14 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -68,7 +68,7 @@ function keyPressed() {
         //random fill
         fill(random(255), random(255), random(255));
         //create circle
-        circle(x, y, z, )
+        circle(x, y, z, s, s)
     }
 }
 
