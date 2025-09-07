102da05f909405efa77756fa6b431b3610923702
jdragonhx <jayden.dhx@gmail.com>
2025-09-07 03:49:09 +0000

    Sep 7, 2025, 3:49 AM

--- L05_kbcontrol.js
+++ L05_kbcontrol.js
@@ -124,6 +124,20 @@ function draw() {
     }
 }
 
+function draw() {
+    background(220);
+    if (showSquare) {
+        rect(width / 2 - 25, height / 2 - 25, 50, 50);
+    }
+}
+
+function draw() {
+    background(220);
+    if (showTriangle) {
+        triangle(width / 2, height / 2 - 25, width / 2 - 25, height / 2 + 25, width / 2 + 25, height / 2 +25);
+    }
+}
+
 function keyPressed() {
     if (key === 'c') {
         showCircle = !showCircle; //Toggle the boolean
