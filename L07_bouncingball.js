3142094a5118acd777024bda242b32f7aa372507
jdragonhx <jayden.dhx@gmail.com>
2025-09-14 04:30:09 +0000

    Sep 14, 2025, 4:30 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -109,7 +109,13 @@ function setup() {
 function draw() {
     background(220)
 
-    if(x+size/2<=0 || x+size )
+    if(x+size/2<=0 || x+size/2=>width) {
+        speedX *=-1;
+    }
+
+    if(y-size/2<=0 || y+size/2>=height) {
+        speedY *=-1;
+    }
 
     x += speedX;
     y += speedY;
