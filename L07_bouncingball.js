9c76a07a8b50b52bf76bfe77c459910bdffa48e2
jdragonhx <jayden.dhx@gmail.com>
2025-09-14 04:05:22 +0000

    Sep 14, 2025, 4:05 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -38,56 +38,57 @@
 
 //recap
 
-let staticImage;
-let soundEffect;
-let x = 200;
-let y = 200;
-let speed = 2;
-let imgwidth = 80;
-let imgheight = 80;
+// let staticImage;
+// let soundEffect;
+// let x = 200;
+// let y = 200;
+// let speed = 2;
+// let imgwidth = 80;
+// let imgheight = 80;
 
-function preload() {
-    staticImage = loadImage('assets/pico-a.png');
-    soundEffect = loadSound('assets/pop.mp3');
-}
+// function preload() {
+//     staticImage = loadImage('assets/pico-a.png');
+//     soundEffect = loadSound('assets/pop.mp3');
+// }
 
-function setup() {
-    createCanvas(400, 400);
-}
+// function setup() {
+//     createCanvas(400, 400);
+// }
 
-function draw() {
-    background('lightblue');
+// function draw() {
+//     background('lightblue');
 
-    x = constrain(x,0, width-imgwidth);
-    y = constrain(y,0, height-imgheight);
+//     x = constrain(x,0, width-imgwidth);
+//     y = constrain(y,0, height-imgheight);
 
-    if(keyIsDown(RIGHT_ARROW)) {
-        x += speed;
-    }
+//     if(keyIsDown(RIGHT_ARROW)) {
+//         x += speed;
+//     }
 
-    if(keyIsDown(LEFT_ARROW)) {
-        x -= speed;
-    }
+//     if(keyIsDown(LEFT_ARROW)) {
+//         x -= speed;
+//     }
 
-    if(keyIsDown(UP_ARROW)) {
-        y -= speed;
-    }
+//     if(keyIsDown(UP_ARROW)) {
+//         y -= speed;
+//     }
 
-    if(keyIsDown(DOWN_ARROW)) {
-        y += speed;
-    }
+//     if(keyIsDown(DOWN_ARROW)) {
+//         y += speed;
+//     }
 
-    image(staticImage, x, y, imgwidth, imgheight);
-}
+//     image(staticImage, x, y, imgwidth, imgheight);
+// }
 
-function keyPressed() {
-    if(keyCode == 32) {
-        imgwidth = 40;
-        soundEffect.play();
-    }
-}
+// function keyPressed() {
+//     if(keyCode == 32) {
+//         imgwidth = 40;
+//         soundEffect.play();
+//     }
+// }
 
-function keyReleased() {
-    imgwidth = 80;
-    soundEffect.pause
-}
+// function keyReleased() {
+//     imgwidth = 80;
+//     soundEffect.pause
+// }
+
