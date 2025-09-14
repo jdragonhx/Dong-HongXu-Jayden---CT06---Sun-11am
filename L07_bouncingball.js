ba0ebdc6580ccb2e247650c376f0620dc5d9d18f
jdragonhx <jayden.dhx@gmail.com>
2025-09-14 03:23:40 +0000

    Sep 14, 2025, 3:23 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -1,35 +1,35 @@
-let x = 200;
-let y = 200;
+// let x = 200;
+// let y = 200;
 
-function setup() {
-    createCanvas(400, 400);
-}
+// function setup() {
+//     createCanvas(400, 400);
+// }
 
-function draw() {
-    background(220);
+// function draw() {
+//     background(220);
 
-    //constrain within the canvas
-    x = constrain(x, 25, width-25)
-    y = constrain(y, 25, height-25)
+//     //constrain within the canvas
+//     x = constrain(x, 25, width-25)
+//     y = constrain(y, 25, height-25)
 
-    if(keyIsDown(RIGHT_ARROW)) {
-        x += 2;
-    }
+//     if(keyIsDown(RIGHT_ARROW)) {
+//         x += 2;
+//     }
 
-    if(keyIsDown(LEFT_ARROW)) {
-        x -= 2;
-    }
+//     if(keyIsDown(LEFT_ARROW)) {
+//         x -= 2;
+//     }
 
 
-    if(keyIsDown(UP_ARROW)) {
-        y -= 2;
-    }
+//     if(keyIsDown(UP_ARROW)) {
+//         y -= 2;
+//     }
 
-    if(keyIsDown(DOWN_ARROW)) {
-        y += 2;
-    }
+//     if(keyIsDown(DOWN_ARROW)) {
+//         y += 2;
+//     }
 
-    fill(0, 0, 255)
+//     fill(0, 0, 255)
 
-    circle(x, y, 50)
-}
+//     circle(x, y, 50)
+// }
