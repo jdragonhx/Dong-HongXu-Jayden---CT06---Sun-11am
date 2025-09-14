326527ea85dc9ee3d04bba35c08b5b5bd9cf8103
jdragonhx <jayden.dhx@gmail.com>
2025-09-14 03:13:24 +0000

    Sep 14, 2025, 3:13 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -9,7 +9,7 @@ function draw() {
     background(220);
 
     //constrain within the canvas
-    x = consrtrain(x, 50)
+    x = consrtrain(x, 25, )
 
     if(keyIsDown(RIGHT_ARROW)) {
         x += 2;
