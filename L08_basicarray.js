ca59216a99caacea3f2aae0626ebad4cc641c390
jdragonhx <jayden.dhx@gmail.com>
2025-09-28 03:39:12 +0000

    Sep 28, 2025, 3:39 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -93,4 +93,10 @@ function setup() {
 
 function draw() {
     background(0)
+
+        text("My favourtie foods are: ", width/2, 50);
+
+    for (let i=0; i<favFoods.length; i++) {
+        text((i+1) + "." + favFoods[i], width/2, 80 + i*50);
+    }
 }
