532eef747b80fcabd27d828eb44347a6c4740076
jdragonhx <jayden.dhx@gmail.com>
2025-09-28 03:55:01 +0000

    Sep 28, 2025, 3:55 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,1 +1,17 @@
-// write your codes here
+let storyText = [
+    "A long time ago in a galaxy far,",
+    "far away...",
+    "",
+    "EPISODE I",
+    "THE BEGINNING",
+    "",
+    "It is a period of learning...",
+    "Students have begun their journey",
+    "into the world of p5.js.",
+    "With newfound powers, they",
+    "create amazing visuals and",
+    "animations...",
+    "",
+    "May the Code be with you!"
+];
+
