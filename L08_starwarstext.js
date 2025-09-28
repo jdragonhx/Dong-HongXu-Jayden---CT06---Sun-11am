34808e611234c65182c90a3a5ff89463777f63ef
jdragonhx <jayden.dhx@gmail.com>
2025-09-28 04:01:47 +0000

    Sep 28, 2025, 4:01 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -38,7 +38,7 @@ function draw() {
         text(storyText[i], width/2, yPos + i*50); 
     }
 
-    if(yPos < -(storyText.length*24 + (storyText)))
+    if(yPos < -(storyText.length*24 + (storyText.length-1)))
 
     yPos -=1; 
 
