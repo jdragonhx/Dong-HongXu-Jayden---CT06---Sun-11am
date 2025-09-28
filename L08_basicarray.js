51940793446ac3245f3ee0651de1679bf20ef724
jdragonhx <jayden.dhx@gmail.com>
2025-09-28 03:57:57 +0000

    Sep 28, 2025, 3:57 AM

--- L08_basicarray.js
+++ L08_basicarray.js
@@ -149,30 +149,30 @@
 
 //Task 4
 
-let favFoods = ["Pizza", "Burger", "Sushi", "Tacos", "Pasta"]; 
-let yPos; 
+// let favFoods = ["Pizza", "Burger", "Sushi", "Tacos", "Pasta"]; 
+// let yPos; 
 
-function setup() {
-    createCanvas(600, 600); 
+// function setup() {
+//     createCanvas(600, 600); 
 
-    fill(255, 255, 0); 
-    textSize(24);
-    textAlign(CENTER); 
+//     fill(255, 255, 0); 
+//     textSize(24);
+//     textAlign(CENTER); 
 
-    yPos = height; 
-}
+//     yPos = height; 
+// }
 
-function draw() {
+// function draw() {
 
-    background(0); 
+//     background(0); 
 
-    //text("My favourite foods are: ", width/2, 50); 
+//     //text("My favourite foods are: ", width/2, 50); 
     
 
-    for (let i=0; i<favFoods.length; i++) {
-        text((i+1) + "." + favFoods[i], width/2, yPos + i*50); 
-    }
+//     for (let i=0; i<favFoods.length; i++) {
+//         text((i+1) + "." + favFoods[i], width/2, yPos + i*50); 
+//     }
 
-    yPos -=1; 
+//     yPos -=1; 
 
-}
+// }
