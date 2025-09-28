3edc6d9fe85d2da176454fb3f1a10a989940040b
jdragonhx <jayden.dhx@gmail.com>
2025-09-28 03:01:57 +0000

    Sep 28, 2025, 3:01 AM

--- L07_bouncingball.js
+++ L07_bouncingball.js
@@ -94,34 +94,46 @@
 
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-let x = 300;
-let y = 200;
-let size = 50;
-let speedX = 1;
-let speedY = 1;
+// let x = 300;
+// let y = 200;
+// let size = 25;
+// let speedX = 1;
+// let speedY = 1;
 
 
-function setup() {
-    createCanvas(600,400);
-    background(220);
-}
+// function setup() {
+//     createCanvas(600,400);
+//     background(220);
+// }
 
-function draw() {
-    // background(220)
+// function draw() {
+//     // background(220)
 
-    if(x-size/2<=0 || x+size/2>=width) {
-        speedX *=-30;
-            //fill(random(255), random(255), random(255))
-    }
+//     if(x-size/2<=0 || x+size/2>=width) {
+//         speedX *=-1;
+//         fill(random(255), random(255), random(255))
+//             //fill(random(255), random(255), random(255))
+//     }
 
-    if(y-size/2<=0 || y+size/2>=height) {
-            //fill(random(255), random(255), random(255))
-        speedY *=-30;
-    }
+//     if(y-size/2<=0 || y+size/2>=height) {
+//             //fill(random(255), random(255), random(255))
+//         speedY *=-1;
+//         fill(random(255), random(255), random(255))
+//     }
 
-    x += speedX;
-    y += speedY;
+//     x += speedX;
+//     y += speedY;
 
-    noStroke();
-    circle(x, y, size);
-}
+//     noStroke();
+//     circle(x, y, size);
+// }
+
+// function keyPressed() {
+// if(keyCode === UP_ARROW) {
+//         speedX *= 1.2;
+//         speedY *= 1.2;
+// }   else if(keyCode === DOWN_ARROW) {
+//         speedX *= 0.8
+//         speedY *= 0.8
+// }
+// }
--- L08_basicarray.js
+++ L08_basicarray.js
@@ -1,1 +1,59 @@
-// write your codes here
+//Task 1
+
+// let x = 300;
+// let y = 200;
+// let size = 30;
+// let speedX = 1;
+// let shapeColor = 255;
+
+// function setup() {
+//     createCanvas(600,400)
+// }
+
+// function draw() {
+//     background(220)
+
+
+//     if(x<=0 || x>=width-size) {
+//         speedX *=-1;
+//         shapeColor = color(random(255), random(255), random(255));
+
+//     }
+
+//     x += speedX;
+//     fill(shapeColor);
+//     noStroke();
+//     rect(x,y,size,size)
+// }
+
+// function keyPressed() {
+// if(keyCode === UP_ARROW) {
+//         speedX *= 1.2;
+//         speedY *= 1.2;
+// }   else if(keyCode === DOWN_ARROW) {
+//         speedX *= 0.8
+//         speedY *= 0.8
+// }
+// }
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//Task 2
+
+function setup() {
+    createCanvas(600,400)
+}
+
+function draw() {
+    background(0);
+    textSize(24)
+    fill(255,255,0);
+    text("My name is usless", 50, 50)
+
+    textSize(16)
+    fill(255,0,0);
+    text("My age is 0", 50, 100);
+    text("My faveourite acticity is getting scolded", 50, 150)
+
+}
+
