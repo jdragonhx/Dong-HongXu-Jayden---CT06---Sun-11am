5354a32d52822543496fd336656b89e47929dd58
jdragonhx <jayden.dhx@gmail.com>
2025-09-28 04:16:48 +0000

    Sep 28, 2025, 4:16 AM

--- L08_starwarstext.js
+++ L08_starwarstext.js
@@ -1,50 +1,50 @@
-let storyText = [
-    "A long time ago in a galaxy far,",
-    "far away...",
-    "",
-    "EPISODE I",
-    "THE BEGINNING",
-    "",
-    "It is a period of learning...",
-    "Students have begun their journey",
-    "into the world of p5.js.",
-    "With newfound powers, they",
-    "create amazing visuals and",
-    "animations...",
-    "",
-    "May the Code be with you!"
-];
+// let storyText = [
+//     "A long time ago in a galaxy far,",
+//     "far away...",
+//     "",
+//     "EPISODE I",
+//     "THE BEGINNING",
+//     "",
+//     "It is a period of learning...",
+//     "Students have begun their journey",
+//     "into the world of p5.js.",
+//     "With newfound powers, they",
+//     "create amazing visuals and",
+//     "animations...",
+//     "",
+//     "May the Code be with you!"
+// ];
 
-let yPos; 
+// let yPos; 
 
-function setup() {
-    createCanvas(600, 600); 
+// function setup() {
+//     createCanvas(600, 600); 
 
-    fill(255, 255, 0); 
-    textSize(24);
-    textAlign(CENTER); 
+//     fill(255, 255, 0); 
+//     textSize(24);
+//     textAlign(CENTER); 
 
-    yPos = height; 
-}
+//     yPos = height; 
+// }
 
-function draw() {
+// function draw() {
 
-    background(0); 
+//     background(0); 
 
-    //text("My favourite foods are: ", width/2, 50); 
+//     //text("My favourite foods are: ", width/2, 50); 
     
 
-    for (let i=0; i<storyText.length; i++) {
-        text(storyText[i], width/2, yPos + i*50); 
-    }
+//     for (let i=0; i<storyText.length; i++) {
+//         text(storyText[i], width/2, yPos + i*50); 
+//     }
 
-    if(yPos < -(storyText.length*24 + (storyText.length-1)*24)) {
-        yPos = height;
-    }
+//     if(yPos < -(storyText.length*24 + (storyText.length-1)*24)) {
+//         yPos = height;
+//     }
 
-    yPos -=1; 
+//     yPos -=1; 
 
-}
+// }
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
