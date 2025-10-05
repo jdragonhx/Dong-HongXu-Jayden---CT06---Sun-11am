a0d39bfb244c8e964333782069babbf2ba8c97ad
jdragonhx <jayden.dhx@gmail.com>
2025-10-05 04:13:04 +0000

    Oct 5, 2025, 4:13 AM

--- L10_name.js
+++ L10_name.js
@@ -1,14 +1,14 @@
 //recap
 
 function setup() {
-    createCanvas(400, 400)
+    createCanvas(400, 400);
     textAlign(CENTER, CENTER);
 }
 
 function draw() {
-    background(220)
+    background(220);
 
-    let s = nf(second(),2)
+    let s = nf(second(),2);
     textSize(50);
     text(s, width/2, height/2);
 
