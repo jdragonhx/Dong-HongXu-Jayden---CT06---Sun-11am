fb22457e2b5c71f5a30bdd29f3e280e93bd3182a
jdragonhx <jayden.dhx@gmail.com>
2025-10-05 03:14:35 +0000

    Oct 5, 2025, 3:14 AM

--- L09_text_alarm.js
+++ L09_text_alarm.js
@@ -1,26 +1,26 @@
 //recap
 
-let x = 300;
-let speed = 1
+// let x = 300;
+// let speed = 1
 
-function setup() {
-    createCanvas(600, 600)
+// function setup() {
+//     createCanvas(600, 600)
 
-    textSize(24);
-    textAlign(LEFT);
+//     textSize(24);
+//     textAlign(LEFT);
 
-}
+// }
 
-function draw() {
-    background(220)
+// function draw() {
+//     background(220)
 
-    text("Bounce!", x, height/2);
+//     text("Bounce!", x, height/2);
 
-    x += speed;
+//     x += speed;
 
-    if(x<=0 || x>= (width-3.5*24)) {
-        speed *= -1
-    }
+//     if(x<=0 || x>= (width-3.5*24)) {
+//         speed *= -1
+//     }
 
-    console.log(x)
-}
+//     console.log(x)
+// }
