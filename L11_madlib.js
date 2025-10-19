da85d3e72f6bf47729fe927a97010b6860a1f1d3
jdragonhx <jayden.dhx@gmail.com>
2025-10-19 03:48:09 +0000

    Oct 19, 2025, 3:48 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -41,8 +41,6 @@ function setup() {
 
     textbox = createInput();
     textBox.position(100, 50);
-    
-
 
     button = createButton('Submit');
     button.position(textBox.x + textBox.width + 30, 50);
