4f1a78999c064c23c3694dbcd658e836a372383b
jdragonhx <jayden.dhx@gmail.com>
2025-11-02 03:45:18 +0000

    Nov 2, 2025, 3:45 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -150,7 +150,7 @@ function setup() {
     
     let template = random(storyTemplates)
 
-    storyText = template;
+    storyText = template.replace("{")
 
     textBox1 = createInput(); 
     textBox1.position(width/2-textBox1.width/2, 50); 
