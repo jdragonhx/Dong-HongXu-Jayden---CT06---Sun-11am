a4ef6f44034cce0de7ef47a1c920fe52ab0dc392
jdragonhx <jayden.dhx@gmail.com>
2025-11-02 04:27:47 +0000

    Nov 2, 2025, 4:27 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -20,6 +20,8 @@ function setup() {
     button = createButton('Guess')
     button.position(width/2-textBox.width/2 + textBox.width + 30, 200)
     button.mousePressed(display);
+
+    
 }
 
 function draw() {
