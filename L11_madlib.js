ab8414368e35a9682eb5c45f1142a1b60cc52d70
jdragonhx <jayden.dhx@gmail.com>
2025-11-02 03:42:23 +0000

    Nov 2, 2025, 3:42 AM

--- L11_madlib.js
+++ L11_madlib.js
@@ -59,75 +59,75 @@
 
 ///////////////////////////////////////////////
 
-let textBox1, textBox2, textBox3, textBox4, textBox5;
-let arr = ["Enter a noun:", "Enter a verb:", "Enter an adjective:", "Enter an adverb:", "Enter a place:"]; 
-let button; 
-let displayText1, displayText2, displayText3, displayText4, displayText5; 
-let arr2 = []; 
+// let textBox1, textBox2, textBox3, textBox4, textBox5;
+// let arr = ["Enter a noun:", "Enter a verb:", "Enter an adjective:", "Enter an adverb:", "Enter a place:"]; 
+// let button; 
+// let displayText1, displayText2, displayText3, displayText4, displayText5; 
+// let arr2 = []; 
 
 
 
-function setup() {
-    createCanvas(600, 600); 
+// function setup() {
+//     createCanvas(600, 600); 
 
-    textBox1 = createInput(); 
-    textBox1.position(width/2-textBox1.width/2, 50); 
-    textBox1.size(150, 30); 
+//     textBox1 = createInput(); 
+//     textBox1.position(width/2-textBox1.width/2, 50); 
+//     textBox1.size(150, 30); 
 
-    textBox2 = createInput(); 
-    textBox2.position(width/2-textBox2.width/2, 100); 
-    textBox2.size(150, 30); 
+//     textBox2 = createInput(); 
+//     textBox2.position(width/2-textBox2.width/2, 100); 
+//     textBox2.size(150, 30); 
 
-    textBox3 = createInput(); 
-    textBox3.position(width/2-textBox3.width/2, 150); 
-    textBox3.size(150, 30); 
+//     textBox3 = createInput(); 
+//     textBox3.position(width/2-textBox3.width/2, 150); 
+//     textBox3.size(150, 30); 
 
-    textBox4 = createInput(); 
-    textBox4.position(width/2-textBox4.width/2, 200); 
-    textBox4.size(150, 30); 
+//     textBox4 = createInput(); 
+//     textBox4.position(width/2-textBox4.width/2, 200); 
+//     textBox4.size(150, 30); 
 
-    textBox5 = createInput(); 
-    textBox5.position(width/2-textBox5.width/2, 250); 
-    textBox5.size(150, 30); 
+//     textBox5 = createInput(); 
+//     textBox5.position(width/2-textBox5.width/2, 250); 
+//     textBox5.size(150, 30); 
 
-    button = createButton('Generate Story'); 
-    button.position(width/2 - button.width/2,300); 
-    button.mousePressed(display); 
+//     button = createButton('Generate Story'); 
+//     button.position(width/2 - button.width/2,300); 
+//     button.mousePressed(display); 
 
-}
+// }
 
-function draw() {
-    background(220); 
-    textSize(15); 
-    for(let i=0; i<arr.length; i++) {
-        text(arr[i], 50, 50+i*50 + 20);
-    }
+// function draw() {
+//     background(220); 
+//     textSize(15); 
+//     for(let i=0; i<arr.length; i++) {
+//         text(arr[i], 50, 50+i*50 + 20);
+//     }
 
-    for(let i=0; i<arr2.length; i++) {
-        text(arr2[i], 50, 350 + i*50); 
-    }
+//     for(let i=0; i<arr2.length; i++) {
+//         text(arr2[i], 50, 350 + i*50); 
+//     }
     
     
-}
+// }
 
-function display() {
-    displayText1 = "Noun: " + textBox1.value(); 
-    arr2.push(displayText1); 
+// function display() {
+//     displayText1 = "Noun: " + textBox1.value(); 
+//     arr2.push(displayText1); 
 
-    displayText2 = "Verb: " + textBox2.value(); 
-    arr2.push(displayText2); 
+//     displayText2 = "Verb: " + textBox2.value(); 
+//     arr2.push(displayText2); 
 
-    displayText3 = "Adjective: " + textBox3.value(); 
-    arr2.push(displayText3); 
+//     displayText3 = "Adjective: " + textBox3.value(); 
+//     arr2.push(displayText3); 
 
-    displayText4 = "Adverb: " + textBox4.value(); 
-    arr2.push(displayText4); 
+//     displayText4 = "Adverb: " + textBox4.value(); 
+//     arr2.push(displayText4); 
 
-    displayText5 = "Place: " + textBox5.value(); 
-    arr2.push(displayText5); 
+//     displayText5 = "Place: " + textBox5.value(); 
+//     arr2.push(displayText5); 
 
 
-}
+// }
 /////////////////////////////////////////////////
 
 
@@ -136,3 +136,4 @@ let storyTemplates = [
         "One day, a {adj} {noun} wanted to {verb} {adv} in {place}.",
         "Did you hear about the {adj} {noun} that tried to {verb} {adv} near {place}?"
     ];
+
