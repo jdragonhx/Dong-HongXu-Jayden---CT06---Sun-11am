bdcff6e63b076cf6c61f615816056698fa03f920
jdragonhx <jayden.dhx@gmail.com>
2025-11-02 04:24:05 +0000

    Nov 2, 2025, 4:24 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -17,7 +17,7 @@ function setup() {
     textBox.position(width/2-textBox.width/2, 200)
 
     buttton = createButton('Guess')
-    button.position(width/2+textBox.width + 50, 200)
+    button.position(width/2+textBox.width + 20, 200)
 }
 
 function draw() {
