98c50dc3e7731df9dddba5bdb55b324ac4095a72
jdragonhx <jayden.dhx@gmail.com>
2025-11-02 04:33:22 +0000

    Nov 2, 2025, 4:33 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -22,7 +22,7 @@ function setup() {
     button.mousePressed(display);
 
     randWord = random(words)
-    randWord = randWord[0].toUpperCase() + " " + "_ ".repeat
+    randWord = randWord[0].toUpperCase() + " " + "_ ".repeat(randWord.length)
 }
 
 function draw() {
