97848c956c4ce521be044064be7e3d4870df7237
jdragonhx <jayden.dhx@gmail.com>
2025-11-02 04:27:03 +0000

    Nov 2, 2025, 4:27 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -30,6 +30,7 @@ function draw() {
     text("Guess the Word!", width/2, 50);
     text("Attempts: " + attempts, width/2, 100);
     text("Hint: " + randWord, width/2, 150);
+    text(textDis)
 }
 
 function display() {
