5e8abc1f4b97ddb271e93732187b848919523676
jdragonhx <jayden.dhx@gmail.com>
2025-11-10 10:07:25 +0000

    Nov 10, 2025, 10:07 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -57,7 +57,7 @@ function checkGuess() {
 function getCorrectLetters(guess, randWord) {
     let correctLetters = "";
     for(let i=0; i<randWord.length; i++) {
-        if(randWord.includes(guess[i])) {
+        if(randWord.includes(guess[i]) ) {
             correctLetters += guess[i].toUpperCase();
         }
     }
