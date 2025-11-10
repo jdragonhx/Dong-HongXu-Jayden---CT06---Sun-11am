7f3352e52627374779c4658259beff66f87cce3b
jdragonhx <jayden.dhx@gmail.com>
2025-11-10 10:32:08 +0000

    Nov 10, 2025, 10:32 AM

--- L13_wordscramblegame.js
+++ L13_wordscramblegame.js
@@ -1,6 +1,7 @@
-let words = ["apple", "mango", "grape", "peach", "melon", "table", "chair", "brick", "cloud", "storm", 
-    "beach", "river", "flame", "grass", "skull", "horse", "laser", "brain", "plane", "train"];
-
+let words = [
+    "elephant", "backpack", "keyboard", "hospital", "sunlight", "raincoat", "notebook", "shoulder", 
+    "football", "bathroom", "sandwich", "airplane", "umbrella", "medicine", "chocolate", "software", 
+    "pineapple", "furniture", "telephone", "lighthouse"];
 let attempts = 0;
 let randWord = "";
 let textBox;
