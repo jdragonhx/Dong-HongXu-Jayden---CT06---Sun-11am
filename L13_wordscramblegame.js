1e99b8df8c47608fe2d72b82dc48bf33e3cb9efd
jdragonhx <jayden.dhx@gmail.com>
2025-11-10 10:41:50 +0000

    Nov 10, 2025, 10:41 AM

--- L13_wordscramblegame.js
+++ L13_wordscramblegame.js
@@ -30,7 +30,7 @@ function setup() {
     buttonS = createButton('Rescramble')
     buttonS.position(width/2-textBox.width/2 + textBox.width + 30, 200)
     buttonS.mousePressed(sameScramble);
-    buttonS.style("font-size", "20px")
+    buttonS.style("font-size", "16px")
     button.size(100, 30);
 
     randWord = random(words)
