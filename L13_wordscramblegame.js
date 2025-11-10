4ce149cd57e3b8d824dc95fc0079722bc739bf4d
jdragonhx <jayden.dhx@gmail.com>
2025-11-10 10:31:25 +0000

    Nov 10, 2025, 10:31 AM

--- L13_wordscramblegame.js
+++ L13_wordscramblegame.js
@@ -1,1 +1,70 @@
-// write your codes here
+
+
+let words = ["apple", "mango", "grape", "peach", "melon", "table", "chair", "brick", "cloud", "storm", 
+    "beach", "river", "flame", "grass", "skull", "horse", "laser", "brain", "plane", "train"];
+
+let attempts = 0;
+let randWord = "";
+let textBox;
+let button;
+let textDis;
+let message = "";
+let hint = "";
+
+function setup() {
+    createCanvas(600, 400)
+
+    textBox = createInput()
+    textBox.size(250, 30);
+    textBox.position(width/2-textBox.width/2, 200)
+    textBox.style("font-size", "24px");
+    textBox.style("background-color", "lightblue");
+
+    button = createButton('Guess')
+    button.position(width/2-textBox.width/2 + textBox.width + 30, 200)
+    button.mousePressed(checkGuess);
+    randWord = random(words)
+    console.log(randWord)
+
+    hint = randWord[0].toUpperCase() + " " + "_ ".repeat(randWord.length-1)
+}
+
+function draw() {
+    background(220)
+
+    textAlign(CENTER, CENTER);
+    textSize(24);
+    text("Guess the Word!", width/2, 50);
+    text("Attempts: " + attempts, width/2, 100);
+    text("Hint: " + hint, width/2, 150);
+    text(message, width/2, 300);
+}
+
+// function display() {
+//     textDis = textBox.value();
+// }
+
+function checkGuess() {
+    attempts++;
+
+    let guess = textBox.value().toLowerCase();
+    message = getCorrectLetters(guess, randWord)
+
+    if(guess == randWord) {
+        message = "You guessed it right! The word is " + randWord;
+    } else if (guess.length !=5) {
+        message = "5 Letters Only!"
+    } else {
+        message = "Try Again. The letters you got right is/are " + message
+    }
+}
+
+function getCorrectLetters(guess, randWord) {
+    let correctLetters = "";
+    for(let i=0; i<randWord.length; i++) {
+        if(randWord.includes(guess[i]) && !correctLetters.includes(guess[i].toUpperCase())) {
+            correctLetters += guess[i].toUpperCase();
+        }
+    }
+    return correctLetters;
+}
