7d8a679cb68f3c495b97f0d269e8021c99ed1f8c
jdragonhx <jayden.dhx@gmail.com>
2025-11-10 10:52:26 +0000

    Nov 10, 2025, 10:52 AM

--- L13_wordscramblegame.js
+++ L13_wordscramblegame.js
@@ -63,7 +63,8 @@ function checkGuess() {
 
     if(guess == hiddenWord) {
         score++;
-        message = "You guessed it right! The word is " + hiddenWord; 
+        message = "You guessed it right! The word is " + hiddenWord;
+        words.splice  
     } else if (guess.length != 5) {
         message = "5 Letters Only!"
     } else {
