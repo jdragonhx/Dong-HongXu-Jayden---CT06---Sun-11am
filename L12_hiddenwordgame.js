d9067075a15c7cc336c41bcd0dfd4e09aafa934c
jdragonhx <jayden.dhx@gmail.com>
2025-11-10 09:58:09 +0000

    Nov 10, 2025, 9:58 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -22,7 +22,7 @@ function setup() {
 
     button = createButton('Guess')
     button.position(width/2-textBox.width/2 + textBox.width + 30, 200)
-    button.mousePressed(display);
+    button.mousePressed(guess);
 
     randWord = random(words)
     randWord = randWord[0].toUpperCase() + " " + "_ ".repeat(randWord.length-1)
