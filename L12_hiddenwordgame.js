bfc4b5d0aa6771dc15fecda4581bffba10164648
jdragonhx <jayden.dhx@gmail.com>
2025-11-10 10:16:09 +0000

    Nov 10, 2025, 10:16 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -53,7 +53,7 @@ function checkGuess() {
     message = getCorrectLetters(guess, randWord)
 
     if(guess == randWord) {
-        message = "You guessed it right! The word is" 
+        message = "You guessed it right! The word is" + randWord;
     }
 }
 
