a581e00aa423bb42064a6dedb0d699ef8a844d02
jdragonhx <jayden.dhx@gmail.com>
2025-11-10 10:49:54 +0000

    Nov 10, 2025, 10:49 AM

--- L13_wordscramblegame.js
+++ L13_wordscramblegame.js
@@ -92,7 +92,7 @@ function pickNewWord() {
     hiddenWord = random(words); 
     console.log(hiddenWord);
 
-    scrambledWord = Scramble
+    scrambledWord = Scramble(hiddenWord);
 }
 
 function sameScramble() {
