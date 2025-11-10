dcf255a9ff3cc1d597dab43b6f30fc03b3072ece
jdragonhx <jayden.dhx@gmail.com>
2025-11-10 09:49:10 +0000

    Nov 10, 2025, 9:49 AM

--- L12_hiddenwordgame.js
+++ L12_hiddenwordgame.js
@@ -1,41 +1,41 @@
-// write your codes here
+// // write your codes here
 
-let words = [
-    "elephant", "backpack", "keyboard", "hospital", "sunlight", "raincoat", "notebook", "shoulder", 
-    "football", "bathroom", "sandwich", "airplane", "umbrella", "medicine", "chocolate", "software", 
-    "pineapple", "furniture", "telephone", "lighthouse"];
+// let words = [
+//     "elephant", "backpack", "keyboard", "hospital", "sunlight", "raincoat", "notebook", "shoulder", 
+//     "football", "bathroom", "sandwich", "airplane", "umbrella", "medicine", "chocolate", "software", 
+//     "pineapple", "furniture", "telephone", "lighthouse"];
 
-let attempts = 0;
-let randWord = "";
-let textBox;
-let button;
-let textDis;
+// let attempts = 0;
+// let randWord = "";
+// let textBox;
+// let button;
+// let textDis;
 
-function setup() {
-    createCanvas(600, 400)
+// function setup() {
+//     createCanvas(600, 400)
 
-    textBox = createInput()
-    textBox.position(width/2-textBox.width/2, 200)
+//     textBox = createInput()
+//     textBox.position(width/2-textBox.width/2, 200)
 
-    button = createButton('Guess')
-    button.position(width/2-textBox.width/2 + textBox.width + 30, 200)
-    button.mousePressed(display);
+//     button = createButton('Guess')
+//     button.position(width/2-textBox.width/2 + textBox.width + 30, 200)
+//     button.mousePressed(display);
 
-    randWord = random(words)
-    randWord = randWord[0].toUpperCase() + " " + "_ ".repeat(randWord.length-1)
-}
+//     randWord = random(words)
+//     randWord = randWord[0].toUpperCase() + " " + "_ ".repeat(randWord.length-1)
+// }
 
-function draw() {
-    background(220)
+// function draw() {
+//     background(220)
 
-    textAlign(CENTER, CENTER);
-    textSize(24);
-    text("Guess the Word!", width/2, 50);
-    text("Attempts: " + attempts, width/2, 100);
-    text("Hint: " + randWord, width/2, 150);
-    text(textDis, width/2, 250);
-}
+//     textAlign(CENTER, CENTER);
+//     textSize(24);
+//     text("Guess the Word!", width/2, 50);
+//     text("Attempts: " + attempts, width/2, 100);
+//     text("Hint: " + randWord, width/2, 150);
+//     text(textDis, width/2, 250);
+// }
 
-function display() {
-    textDis = textBox.value();
-}
+// function display() {
+//     textDis = textBox.value();
+// }
